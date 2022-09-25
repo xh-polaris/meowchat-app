174532543c2d88efeea29d1b162048b052b679ec
foliet <foliet@rainspace.cn>
2022-09-25 16:12:15 +0800

    chore: 初始化工程目录

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+    env: {
+        browser: true,
+        node: true,
+        es2021: true
+    },
+    extends: [
+        "plugin:vue/vue3-essential",
+        "@vue/typescript/recommended"
+    ],
+    overrides: [],
+    parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module"
+    },
+    plugins: [
+        "vue",
+    ],
+    rules: {
+        quotes: [
+            "error",
+            "double",
+        ],
+        "vue/multi-word-component-names": "off"
+    }
+}
---
+++ src/pages/index/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <view class="content">
+    <image class="logo" src="/static/logo.png"/>
+    <view class="text-area">
+      <text class="title">{{ title }}</text>
+    </view>
+  </view>
+</template>
+
+<script setup lang="ts">
+import { ref, onMounted } from "vue"
+
+const title = ref("Hello")
+onMounted(() => console.log(1))
+</script>
+
+<style>
+.content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.logo {
+  height: 200 rpx;
+  width: 200 rpx;
+  margin: 200 rpx auto 50 rpx;
+}
+
+.text-area {
+  display: flex;
+  justify-content: center;
+}
+
+.title {
+  font-size: 36 rpx;
+  color: #8f8f94;
+}
+</style>
