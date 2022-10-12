099f96f41bff07049cb4e470196d67438b1b560c
马驹子 <2318332846@qq.com>
2022-10-12 11:26:53 +0800

    feature/collection/ui

--- src/components/catsbox/catsbox.vue
+++ src/components/catsbox/catsbox.vue
@@ -6,14 +6,19 @@
     <view class="text">
       <view class="title">
         <text>{{item.title}}</text>
+        <view class="collect">
+          <image :src="item.collect_src" mode="aspectFill" ></image>
+        </view>
       </view>
         <view class="data">
           <text>花色：{{item.color}}</text>
         </view>
         <view class="data"> 
          <text>出没地区： {{item.place}}</text>
-        </view>             
+        </view>
+
    </view>
+
   </view>
 </template>
 
@@ -28,7 +33,9 @@ export default {
           title:"默认猫咪",
           color:"无色",
           place:"中北校区",
-          picture:"../../static/image/dog0.jpg"
+          picture:"../../static/image/dog0.jpg",
+          collect_src:"../../static/image/collect.png"
+
         }
       }
     }
@@ -59,7 +66,7 @@ image{
 }
 }
 .text{
-  flex:1;
+  //flex:1;
   padding-left:20rpx;
   margin-left: 40rpx;
   //display: flex;
@@ -69,7 +76,7 @@ image{
   font-size: 40rpx;
   color:#333;
   margin-top: 20rpx;
-  margin-bottom: 60rpx;
+  margin-bottom: 10rpx;
   //text-overflow: -o-ellipsis-lastline;
   overflow: hidden;				//溢出内容隐藏
 text-overflow: ellipsis;		//文本溢出部分用省略号表示
@@ -77,6 +84,12 @@ display: -webkit-box;			//特别显示模式
 -webkit-line-clamp: 2;			//行数
 line-clamp: 2;
   -webkit-box-orient: vertical;	//盒子中内容竖直排列
+  image{
+    padding-left: 300rpx;
+    margin-top: 10rpx;
+    width:40rpx;
+    height: 40rpx;
+  }
 }
 .data{
   font-size: 25rpx;
@@ -84,6 +97,8 @@ line-clamp: 2;
  margin-bottom: 10rpx;
 
 }
+
 }
+
 }
 </style>
