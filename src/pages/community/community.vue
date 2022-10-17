a00acbb29527c0cf320603c0515689adfdbe4e48
chachachachachaxxxxx <205373694@qq.com>
2022-10-17 12:18:41 +0800

    feat:喵友圈添加了轮播器（有待完善）

--- src/pages/community/community.vue
+++ src/pages/community/community.vue
@@ -1,24 +1,58 @@
 <template>
-  <view class="content">
-    <image class="logo" src="/static/logo.png" @click="onClickImage" />
-    <view class="text-area">
-      <text class="title">
-        {{ title }}
-      </text>
-    </view>
+  <view class="uni-margin-wrap">
+    <swiper
+      :autoplay="true" :indicator-dots="true"
+      :interval="2000"
+      circular class="swiper"
+    >
+      <swiper-item>
+        <view class="swiper-item" style="background:blue">
+          A
+        </view>
+      </swiper-item>
+      <swiper-item>
+        <view class="swiper-item" style="background:green">
+          B
+        </view>
+      </swiper-item>
+      <swiper-item>
+        <view class="swiper-item" style="background:blue">
+          C
+        </view>
+      </swiper-item>
+    </swiper>
   </view>
 </template>
 
 <script lang="ts" setup>
-import { ref } from "vue"
+import {ref} from "vue";
 
-const title = ref("Hello")
+const title = ref("Hello");
 const onClickImage = () => {
-  uni.navigateTo({ url: "/pages/moment/moment" })
-}
+  uni.navigateTo({url: "/pages/moment/moment"});
+};
 </script>
+<style>
+.uni-margin-wrap {
+  width: 690 rpx;
+  width: 100%;
+}
+
+.swiper {
+  height: 20vh;
+}
+
+.swiper-item {
+  display: block;
+  height: 20vh;
+  line-height: 15vh;
+  text-align: center;
+}
+
+</style>
 
 <style lang="scss" scoped>
+
 .content {
   align-items: center;
   display: flex;
