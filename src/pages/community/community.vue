263f1593183af2aa1382d69783654835e16d3e72
foliet <foliet@rainspace.cn>
2022-10-24 14:18:12 +0800

    feat: 为喵友圈添加瀑布屏和轮播器 (#6)
    * feat:喵友圈添加了轮播器（有待完善）
    
    * feat:添加了nvue的瀑布流组件
    
    * feat:瀑布流布局外观部分demo做成
    
    Co-authored-by: chachachachachaxxxxx <205373694@qq.com>

--- src/pages/community/community.vue
+++ src/pages/community/community.vue
@@ -1,24 +1,66 @@
 <template>
-  <view class="content">
-    <image class="logo" src="/static/logo.png" @click="onClickImage" />
-    <view class="text-area">
-      <text class="title">
-        {{ title }}
-      </text>
+  <!--  -->
+  <view>
+    <view class="uni-margin-wrap">
+      <swiper
+        :autoplay="true" :indicator-dots="true"
+        :interval="2000"
+        circular class="swiper"
+      >
+        <swiper-item>
+          <view class="swiper-item" style="background:skyblue">
+            A
+          </view>
+        </swiper-item>
+        <swiper-item>
+          <view class="swiper-item" style="background:green">
+            B
+          </view>
+        </swiper-item>
+        <swiper-item>
+          <view class="swiper-item" style="background:pink">
+            C
+          </view>
+        </swiper-item>
+      </swiper>
     </view>
+  </view>
+  <view style="margin-top:10px">
+    <water-fall />
+    <view />
   </view>
 </template>
 
 <script lang="ts" setup>
-import { ref } from "vue"
+import {ref} from "vue";
+import WaterFall from "@/pages/community/waterFall.vue";
 
-const title = ref("Hello")
+const list = ref(["A", "B", "C", "D", "E"]);
+const title = ref("Hello");
+
 const onClickImage = () => {
-  uni.navigateTo({ url: "/pages/moment/moment" })
-}
+  uni.navigateTo({url: "/pages/moment/moment"});
+};
+
 </script>
 
 <style lang="scss" scoped>
+.uni-margin-wrap {
+  width: 690rpx;
+  width: 100%;
+}
+
+.swiper {
+  height: 20vh;
+}
+
+.swiper-item {
+  display: block;
+  height: 20vh;
+  line-height: 15vh;
+  text-align: center;
+}
+
 .content {
   align-items: center;
   display: flex;
