52c2412fdcaec2721440da0acdf17c94f4d9243e
xx <78578210+chachachachachaxxxxx@users.noreply.github.com>
2022-10-24 22:43:56 +0800

    feat: 进一步完善喵友圈页面
    * feat:喵友圈添加了轮播器（有待完善）
    
    * feat:添加了nvue的瀑布流组件
    
    * feat:瀑布流布局外观部分demo做成
    
    * refactor:重构了轮播器部分的代码，将控制部分添加到了script中
    
    * feat:完善了页面顶部一些关于学校信息外观的css
    
    * fix: 清除黄色警告

--- src/pages/community/community.vue
+++ src/pages/community/community.vue
@@ -1,53 +1,91 @@
 <template>
   <!--  -->
-  <view>
-    <view class="uni-margin-wrap">
-      <swiper
-        :autoplay="true" :indicator-dots="true"
-        :interval="2000"
-        circular class="swiper"
-      >
-        <swiper-item>
-          <view class="swiper-item" style="background:skyblue">
-            A
-          </view>
-        </swiper-item>
-        <swiper-item>
-          <view class="swiper-item" style="background:green">
-            B
-          </view>
-        </swiper-item>
-        <swiper-item>
-          <view class="swiper-item" style="background:pink">
-            C
-          </view>
-        </swiper-item>
-      </swiper>
+  <view class="school-box">
+    <view class="school-select-box">
+      <view class="school-name">
+        {{ school.name }}
+      </view>
+      <view class="switch-box">
+        <button plain="true" style="width:100px; font-size:small; margin:10px 10px;" type="primary">
+          切换学校
+        </button>
+      </view>
+    </view>
+    <view class="school-select-box">
+      <view v-for="item in school.campuses" :key="item">
+        {{ item }}
+      </view>
     </view>
   </view>
+
+  <view class="swiper-box">
+    <swiper
+      :autoplay="swiper.autoplay" :duration="swiper.duration"
+      :indicator-dots="swiper.indicatorDots" :interval="swiper.interval"
+      circular class="swiper"
+    >
+      <swiper-item v-for="item in list" :key="item">
+        <view class="swiper-item" style="background:skyblue">
+          {{ item }}
+        </view>
+      </swiper-item>
+    </swiper>
+  </view>
+
   <view style="margin-top:10px">
     <water-fall />
-    <view />
   </view>
 </template>
 
 <script lang="ts" setup>
-import {ref} from "vue";
+import {reactive, ref} from "vue";
 import WaterFall from "@/pages/community/waterFall.vue";
 
-const list = ref(["A", "B", "C", "D", "E"]);
-const title = ref("Hello");
+const school = reactive({
+  name: "华东师范大学",
+  campuses: ["中北校区", "闵行校区", "不限"],
+  No: 0
+});
 
-const onClickImage = () => {
-  uni.navigateTo({url: "/pages/moment/moment"});
-};
+const swiper = reactive({
+  // 依次为 自动播放、是否显示面板指示点、自动切换时长、滑动动画时长
+  autoplay: true,
+  indicatorDots: true,
+  interval: 2000,
+  duration: 500,
+});
+
+const list = ref(["A", "B", "C", "D", "E"]);
+// const title = ref("Hello");
 
 </script>
 
 <style lang="scss" scoped>
-.uni-margin-wrap {
-  width: 690rpx;
-  width: 100%;
+.switch-box {
+  margin-left: auto;
+}
+
+.school-box {
+  height: 12vh;
+  display: flex;
+  flex-direction: column;
+}
+
+.school-select-box {
+  height: 8vh;
+  display: flex;
+  flex-direction: row
+}
+
+.school-name {
+  margin: 10px 20px;
+  font-weight: bold;
+  border-bottom: 4px solid skyblue;
+  font-size: large;
+}
+
+.swiper-box {
+  width: 100vw;
 }
 
 .swiper {
