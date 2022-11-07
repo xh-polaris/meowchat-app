c1eddd8b56e14762c0e6efc03bd50dd1231945d3
foliet <foliet@rainspace.cn>
2022-11-07 21:08:53 +0800

    chore: 去掉无用代码注释

--- src/pages/community/water-fall.vue
+++ src/pages/community/water-fall.vue
@@ -15,28 +15,23 @@
         <view>时间</view>
       </view>
     </view>
-    <!--返回顶部-->
-    <!--    <view :style="{'display':(flag===true? 'block':'none')}" class="top">-->
-    <!--      <image class="topc" src="../../static/top.png" @click="top" />-->
-    <!--    </view>-->
   </view>
 </template>
 
 <script setup>
-import {ref} from "vue";
+import { ref } from "vue"
 
-const imgList = ref(["collect.png", "collect_HL.png", "collection.png"]);
-const srcURL = ref("/static/images/");
-// const src = ref("https://bjetxgzv.cdn.bspapp.com/VKCEYUGU-uni-app-doc/6acec660-4f31-11eb-a16f-5b3e54966275.jpg");
+const imgList = ref(["collect.png", "collect_HL.png", "collection.png"])
+const srcURL = ref("/static/images/")
 
 const clickImg = (event) => {
-  let imgURL = event.currentTarget.dataset.src;
-  let currentUrl = event.currentTarget.dataset.src;   //获取点击图片的地址, **对应<template>里面的 :data-src="item.src"
+  let imgURL = event.currentTarget.dataset.src
+  let currentUrl = event.currentTarget.dataset.src   //获取点击图片的地址, **对应<template>里面的 :data-src="item.src"
   uni.previewImage({
     urls: [imgURL],    //这里是单图 . 需要预览的全部图片地址,这个数组是必须的,要用[]
     current: currentUrl, //当前显示图片的地址
-  });
-};
+  })
+}
 </script>
 
 <style>
@@ -75,18 +70,5 @@ const clickImg = (event) => {
   text-align: right;
 }
 
-/* 回到顶部
-.top {
-  position: relative;
-  display: none; /* 先将元素隐藏
-}
-
-.topc {
-  height: 30px;
-  width: 30px;
-  position: fixed;
-  right: 0;
-  top: 70%;
-}*/
 </style>
 
