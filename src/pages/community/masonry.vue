a1cce644a3c1062b735b630552e931dcedd75863
oustr <biu_biu_@outlook.com>
2022-11-19 23:05:53 +0800

    refactor: 按照设计需求调整瀑布流样式

--- src/pages/community/masonry.vue
+++ src/pages/community/masonry.vue
@@ -4,7 +4,16 @@
     <block v-for="(tile, index) in leftTiles" :key="tile.index">
       <view class="tile">
         <image mode="widthFix" :src="tile.url" class="img" @load.once="tile.onLoad"></image>
-        <view class="description">{{tile.title}}</view>
+        <view class="tile-info">
+          <view class="title">{{tile.title}}</view>
+          <view class="other-info">
+            <view class="user-info">
+              <view class="avatar"></view>
+              <view class="username">Username</view>
+            </view>
+            <view class="time">1小时前</view>
+          </view>
+        </view>
       </view>
     </block>
   </view>
@@ -12,7 +21,16 @@
     <block v-for="(tile, index) in rightTiles" :key="tile.index">
       <view class="tile">
         <image mode="widthFix" :src="tile.url" class="img" @load.once="tile.onLoad"></image>
-        <view class="description">{{tile.title}}</view>
+        <view class="tile-info">
+          <view class="title">{{tile.title}}</view>
+          <view class="other-info">
+            <view class="user-info">
+              <view class="avatar"></view>
+              <view class="username">Username</view>
+            </view>
+            <view class="time">1小时前</view>
+          </view>
+        </view>
       </view>
     </block>
   </view>
@@ -121,41 +139,83 @@ addBatch()
 
 <style lang="scss" scoped>
 
-$gapWidth: 3vw;
+$sideMargin: 12/390*100vw;
+$horizontalGap: 8/390*100vw;
+$verticalGap: 10/390*100vw;
+
+$radius: 6/390*100vw;
+$titleFontSize: 12/390*100vw;
+$smallFontSize: 8/390*100vw;
+$avatarWidth: 21/390*100vw;
 
 .masonry {
   display: flex;
   background-color: transparent;
-  padding-top: $gapWidth;
+  padding-top: 10vw;
 }
 .column-left {
-  width: calc(50vw - $gapWidth*1.5);
-  margin-left: $gapWidth;
-  margin-right: calc($gapWidth/2);
+  width: calc(50vw - $sideMargin - $horizontalGap/2);
+  margin-left: $sideMargin;
+  margin-right: calc($horizontalGap/2);
   height: fit-content;
 }
 .column-right {
-  width: calc(50vw - $gapWidth*1.5);
-  margin-left: calc($gapWidth/2);
-  margin-right: $gapWidth;
+  width: calc(50vw - $sideMargin - $horizontalGap/2);
+  margin-left: calc($horizontalGap/2);
+  margin-right: $sideMargin;
   height: fit-content;
 }
 
 .tile {
-  margin-bottom: calc($gapWidth - 2px);
+  margin-bottom: calc($verticalGap - 2px);
+  box-shadow: 0 0 5px -1px rgba(0, 0, 0, 0.25);
+  border-radius: $radius;
+  font-family: sans-serif;
   
   .img {
-    width: calc(50vw - $gapWidth*1.5);
+    width: calc(50vw - $sideMargin - $horizontalGap/2);
     display: block;
-    border-radius: 4vw 4vw 0 0;
+    border-radius: $radius $radius 0 0;
   }
-  .description {
-    background-color: #BBB;
-    font-size: 4vw;
-    padding: 2vw;
-    border-radius: 0 0 4vw 4vw;
+
+  .tile-info {
     transform: translateY(-2px);
+    background-color: #FFFFFF;
+    border-radius: 0 0 $radius $radius;
+    font-size: $smallFontSize;
+    color: #696969;
+    font-weight: 500;
+    padding: 12/390*100vw;
+    padding-bottom: 6/390*100vw;
+
+    .title {
+      font-size: $titleFontSize;
+      color: #000000;
+      line-height: $titleFontSize;
+      margin-bottom: 12/390*100vw;
+    }
+
+    .other-info {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+
+      .user-info {
+        display: flex;
+        align-items: center;
+
+        .avatar {
+          width: $avatarWidth;
+          height: $avatarWidth;
+          border-radius: 50%;
+          background-color: #CCCCCC;
+          margin-right: 4/390*100vw;
+        }
+      }
+    }
   }
+
+
 }
 
 .debug {
