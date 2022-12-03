5219a2cfe772aacd044c217a0d9aaa96692a47ca
Yufeng Cheng <1351416566@qq.com>
2022-12-04 01:13:13 +0800

    feat: 添加施工中组件

---
+++ src/components/Developing/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <view class="container">
+        <image src="https://static.xhpolaris.com/dog.jpg" />
+    </view>
+</template>
+  
+<style lang="scss" scoped>
+  .container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: 20vh;
+  }
+</style>
+  
