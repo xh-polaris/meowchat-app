18f2df06cb802534df9fafd0a74937cbe48f27b3
JYcx9248 <10205101463@stu.ecnu.edu.cn>
2022-12-07 19:32:15 +0800

    feat: 完成部分profile页面 (#69)

---
+++ src/pages/profile/event.ts
@@ -0,0 +1,10 @@
+export function CopyToClipboard(res :string){
+	  uni.setClipboardData({
+	                    data: res,
+	                    success: () => {
+	                        uni.showToast({
+	                            title: '复制成功'
+	                        })
+	                    }
+	                })
+}
