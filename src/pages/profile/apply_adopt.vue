86df1f1a0105e341bb16d92f66e10194324e2cac
JYcx9248 <10205101463@stu.ecnu.edu.cn>
2022-12-11 13:03:06 +0800

    fix: 修正profile及其子页面自适应问题 (#73)
    * fix: 修改页面命名不一致问题
    
    * fix: 修正自适应问题
    
    * fix: 修正profile及其子页面自适应问题

--- src/pages/profile/apply_adopt.vue
+++ src/pages/profile/apply_adopt.vue
@@ -1,17 +1,19 @@
 <template>
 	<view class="container">
 		<image class="bg-set" src="https://static.xhpolaris.com/profile_background.png" />
-		<view class="intro">
-			 您好，我们是华东师范大学小动物保护协会
-		</view>
-		<view class="intro1">
-			  感谢您选择领养代替购买，但养猫的实际情况可能会和您想象的不太一样，请扫描下方二维码阅读全部内容，认真考虑是否领养，并如实填写问卷
+	    <view class="intro_wrap">
+			<view class="intro">
+				   您好，我们是华东师范大学小动物保护协会
+			</view>
+			<view class="intro">
+				感谢您选择领养代替购买，但养猫的实际情况可能会和您想象的不太一样，请扫描下方二维码阅读全部内容，认真考虑是否领养，并如实填写问卷
+			</view>
 		</view>
 		<view class="QRcode_content">
-			<image class="QRcode" src="/static/images/qrcode.png"  style=" width: 376rpx;" mode="widthFix" />
+			<image class="QRcode" src="/static/images/qrcode.png"  style=" width: 80%" mode="widthFix" />
 		</view>
 		<view class="link_content">
-			<text class="title">问卷链接：</text>
+			<text class="title">问卷链接:</text>
 			<navigator url="/pages/profile/apply_adopt_questionnaire" hover-class="none">
 			<view class="link">
 				https://www.wjx.cn/vj/mBnSgB8.aspx 
@@ -33,18 +35,15 @@
 	  left: 0;
 	  z-index: -1;
 	}
+	.intro_wrap{
+		margin-top: 50rpx;
+		vertical-align: middle;
+		text-align: center;
+	}
 	.intro {
-	  text-align: center;
-	  margin-top:30rpx;
 	  margin-right: 51rpx;
 	  margin-left:54rpx;
-	  font-size: 30rpx;
-	}
-	.intro1 {
-		text-align: center;
-		margin-right: 51rpx;
-		margin-left:54rpx;
-		font-size: 30rpx;
+	  font-size: 28rpx;
 	}
 	.QRcode_content{
 		box-shadow: 0 0 10rpx #EEEEEE;
@@ -58,26 +57,31 @@
 		text-align: center;
 		.QRcode{
 			margin-top: 100rpx;
-			margin-bottom: 100rpx;	
+			margin-bottom: 100rpx;
+			height: 376rpx;
+			width:376rpx;
 		}
 	}
 	.link_content{
+		height: 36rpx;
+		width: 600rpx;
 		box-shadow: 0 0 10rpx #EEEEEE;
-		margin: 60rpx;
+		margin-left: 60rpx;
 		padding: 20rpx;
 		margin-top:50rpx;
 		margin-bottom: 10rpx;
 		border-radius: 21rpx;
 		background-color: #FFFFFF;	
-		vertical-align: middle;
-		text-align: center;
-		flex-flow: row;
 		display: flex;
 		.title{
+			height:36rpx;
+			width:112rpx;
 			font-weight: bold;
 			font-size: 25rpx;
 		}
 		.link{
+			height:36rpx;
+			width: 440rpx;
 			color: #007BFF;
 			font-weight: bold;
 			font-size: 25rpx;
