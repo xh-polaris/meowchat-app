9b4455370813b21f3f7c88f71810e02a16ee37b1
majuzi123 <98637453+majuzi123@users.noreply.github.com>
2023-01-07 17:02:50 +0800

    fix: 图鉴详情页面做了进一步优化 (#107)

--- src/pages/cat/cat.vue
+++ src/pages/cat/cat.vue
@@ -1,12 +1,12 @@
 <template>
   <view>
-    <image :src="mainImgUrl" class="img" />
+    <image :src="mainImgUrl" class="img"/>
     <view>
       <scroll-view scroll-x="true" class="scroll-view-item_H" scroll-left="120">
         <li
-          v-for="(item,index) in cat.avatars" :key="index"
-          class="img-li"
-          @click="()=>{mainImgUrl= item,imgActiveIndex=index}"
+            v-for="(item,index) in cat.avatars" :key="index"
+            class="img-li"
+            @click="()=>{mainImgUrl= item,imgActiveIndex=index}"
         >
           <img :class="index === imgActiveIndex ? 'img_activeBorder' : ''" :src="item" style="width:50px;height:50px">
         </li>
@@ -19,30 +19,30 @@
 
       <view class="text-box2">
         <progress
-          :percent="cat.popularity"
-          :stroke-width="10"
-          activeColor="#63A4F9"
-          backgroundColor="#F5F5F5"
-          border-radius="6px"
+            :percent="cat.popularity"
+            :stroke-width="10"
+            activeColor="#63A4F9"
+            backgroundColor="#F5F5F5"
+            border-radius="6px"
         />
         <text>{{ cat.popularity }}人气值</text>
       </view>
     </view>
     <view class="info">
       <view class="combination">
-        <view class="attribute"> 年龄 </view>
+        <view class="attribute"> 年龄</view>
         <view class="content">
           {{ cat.age }}
         </view>
       </view>
       <view class="combination">
-        <view class="attribute"> 花色 </view>
+        <view class="attribute"> 花色</view>
         <view class="content">
           {{ cat.color }}
         </view>
       </view>
       <view class="combination">
-        <view class="attribute"> 当前地区 </view>
+        <view class="attribute"> 当前地区</view>
         <view class="area-content">
           {{ cat.area }}
         </view>
@@ -50,42 +50,74 @@
     </view>
     <view class="info">
       <view class="combination">
-        <view class="attribute"> 性别 </view>
+        <view class="attribute"> 性别</view>
         <view class="content">
           {{ cat.sex }}
         </view>
       </view>
       <view class="combination">
-        <view class="attribute"> 绝育情况 </view>
+        <view class="attribute"> 绝育情况</view>
         <view class="content">
           {{ Sterilized }}
         </view>
       </view>
       <view class="combination">
-        <view class="attribute"> 是否剪耳 </view>
+        <view class="attribute"> 是否剪耳</view>
         <view class="content">
           {{ Snipped }}
         </view>
       </view>
     </view>
-    <view class="divider" />
+    <view class="divider"/>
     <view class="photo">
-      <text> 详情 </text>
+      <text class="maopian"> 猫片</text>
       <view class="dd">
         <text class="detail_info">
           {{ cat.details }}
         </text>
+        <view v-if="spread" @click="()=>{spread=!spread}">
+          <text style="font-size: 25rpx; ">点击展开
+          </text>
+          <image src="../../static/images/open.png"
+                 style=" width:25rpx; height: 25rpx; margin-right: 40rpx; "></image>
+        </view>
+        <view v-if="!spread" @click="()=>{spread=!spread}">
+          <text style="font-size: 25rpx; ">点击收起
+          </text>
+          <image src="../../static/images/back.png"
+                 style=" width:25rpx; height: 25rpx; margin-right: 40rpx; "></image>
+        </view>
       </view>
-      <view class="end" />
+      <view v-if="!spread" class="divider1"/>
+      <view v-if="!spread" class="end">
+        <text class="detail_info">
+          {{ cat.details }}
+        </text>
+      </view>
+      <view class="imgs">
+        <text>
+          11月
+        </text>
+        <view class="qz_imgs qz_imgs3 clearfix">
+
+          <li
+              v-for="(item,index) in imgUrlList" :key="index"
+          >
+            <image :src="item" mode="aspectFill"/>
+
+          </li>
+
+        </view>
+      </view>
     </view>
   </view>
 </template>
 
 <script lang="ts" setup>
-import { reactive, ref } from "vue";
-import { Cat } from "@/apis/schemas";
-import { getCatDetail } from "@/apis/collection/collection";
-import { GetCatDetailReq } from "@/apis/collection/collection-interfaces";
+import {reactive, ref} from "vue";
+import {Cat} from "@/apis/schemas";
+import {getCatDetail} from "@/apis/collection/collection";
+import {GetCatDetailReq} from "@/apis/collection/collection-interfaces";
 
 const props = defineProps<{
   id: string;
@@ -93,6 +125,16 @@ const props = defineProps<{
 const getCatDetailReq = reactive<GetCatDetailReq>({
   catId: props.id,
 });
+let imgUrlList = [
+  "https://fuss10.elemecdn.com/a/3f/3302e58f9a181d2509f3dc0fa68b0jpeg.jpeg",
+  "https://fuss10.elemecdn.com/1/34/19aa98b1fcb2781c4fba33d850549jpeg.jpeg",
+  "https://fuss10.elemecdn.com/0/6f/e35ff375812e6b0020b6b4e8f9583jpeg.jpeg",
+  "https://fuss10.elemecdn.com/9/bb/e27858e973f5d7d3904835f46abbdjpeg.jpeg",
+  "https://fuss10.elemecdn.com/d/e6/c4d93a3805b3ce3f323f7974e6f78jpeg.jpeg",
+  "https://fuss10.elemecdn.com/3/28/bbf893f792f03a54408b3b7a7ebf0jpeg.jpeg",
+  "https://fuss10.elemecdn.com/2/11/6535bcfb26e4c79b48ddde44f4b6fjpeg.jpeg",
+
+];
 let Sterilized: string;
 let Snipped: string;
 let cat = reactive<Cat>({
@@ -113,6 +155,7 @@ let cat = reactive<Cat>({
 });
 const mainImgUrl = ref("");
 const imgActiveIndex = ref(0);
+const spread = ref(true);
 getCatDetail(getCatDetailReq).then(res => {
       cat.id = res.cat.id;
       cat.createAt = res.cat.createAt;
@@ -141,6 +184,7 @@ getCatDetail(getCatDetailReq).then(res => {
       mainImgUrl.value = cat.avatars[0];
     }
 );
+
 </script>
 
 <style lang="scss" scoped>
@@ -256,9 +300,38 @@ getCatDetail(getCatDetailReq).then(res => {
   margin-bottom: 20rpx;
 }
 
+.divider1 {
+
+  width: 100%;
+  height: 1rpx;
+  border: 1rpx solid #FFFFFF;
+}
+
 .photo {
-  text-align: center;
+  // text-align: center;
   margin-top: 40rpx;
+
+  .maopian {
+    /* 猫片 */
+
+    //position: absolute;
+    width: 100rpx;
+    height: 100rpx;
+    left: 15rpx;
+    top: 677rpx;
+
+    font-family: 'Abel';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 60rpx;
+    line-height: 41rpx;
+    /* identical to box height, or 205% */
+    letter-spacing: 0.374rpx;
+
+    color: #000000;
+
+
+  }
 
   text {
     color: #212223;
@@ -267,21 +340,28 @@ getCatDetail(getCatDetailReq).then(res => {
   }
 
   .dd {
-    text-align: center;
+    //text-align: center;
     margin-right: auto;
     margin-top: 20rpx;
     width: 100%;
     color: white;
-    background: linear-gradient(90deg, #0688f3, white);
+    //background: linear-gradient(90deg, #0688f3, white);
+    background: #EAF6FF;
+    display: flex;
+    justify-content: space-between;
+    white-space: nowrap;
+    //padding-left: 30rpx;
+    padding-top: 30rpx;
   }
 
   .end {
     text-align: center;
     margin-right: auto;
     width: 100%;
-    height: 30rpx;
+    height: 200rpx;
     color: white;
-    background: linear-gradient(90deg, #0688f3, white);
+    // background: linear-gradient(90deg, #0688f3, white);
+    background: #EAF6FF;
   }
 
   .detail_info {
@@ -295,17 +375,36 @@ getCatDetail(getCatDetailReq).then(res => {
     margin-top: 40rpx;
 
     text {
-      color: #212223;
-      font-size: 30rpx;
-      margin-left: 20rpx;
+
+      /* 11月 */
+
+      //position: absolute;
+      width: 27rpx;
+      height: 17rpx;
+      left: 16rpx;
+
+
+      font-family: 'Inter';
+      font-style: normal;
+      font-weight: 500;
+      font-size: 40rpx;
+      line-height: 17rpx;
+      /* identical to box height */
+      letter-spacing: -0.3rpx;
+
+      /* darkgrey02 */
+      color: #353535;
+
+
     }
 
     .qz_imgs {
       position: relative;
       display: flex;
       overflow: hidden;
-      // justify-content: space-between;
-      // flex-wrap: wrap;
+      //justify-content: space-between;
+      flex-wrap: wrap;
+
       &.qz_imgs3 {
         image {
           width: 212rpx;
@@ -319,4 +418,5 @@ getCatDetail(getCatDetailReq).then(res => {
     }
   }
 }
+
 </style>
