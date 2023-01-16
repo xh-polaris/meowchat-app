810b906ea514b8a28960cb6df636cd7ddba8f002
foliet <foliet@rainspace.cn>
2023-01-17 04:14:12 +0800

    fix: 修复本地时间与服务器时间不一致时，显示的时间可能为“几秒后”的问题

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -2,6 +2,11 @@ import moment from "moment";
 import "moment/dist/locale/zh-cn";
 
 export function displayTime(secTime: number) {
-  const timeStr = moment(secTime * 1000).fromNow();
+  secTime *= 1000;
+  const now = new Date().getTime();
+  if (now < secTime) {
+    secTime = now;
+  }
+  const timeStr = moment(secTime).fromNow();
   return timeStr.replace(" ", "");
 }
