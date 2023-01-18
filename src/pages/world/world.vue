5f2e70c317e21833b4e95d62598ac9e101006616
oustr <biu_biu_@outlook.com>
2023-01-18 16:05:43 +0800

    refactor: 抽象出瀑布流和帖子，用于接下来的搜索功能

--- src/pages/world/world.vue
+++ src/pages/world/world.vue
@@ -20,97 +20,23 @@
 
   <view class="top-padding" />
 
-  <template v-for="post in postsData" :key="post.id">
-    <view class="post" @click="onClickPost(post.id)">
-      <view class="upper">
-        <view :class="'main ' + (post.coverUrl ? 'hasImage' : '')">
-          <view class="title">
-            {{ post.title }}
-          </view>
-          <view class="user-info">
-            <template v-if="!post.isAnonymous">
-              <image :src="post.user.avatarUrl" class="avatar" />
-              <view class="username">
-                {{ post.user.nickname }}
-              </view>
-            </template>
-          </view>
-          <view class="description">
-            {{ post.text }}
-          </view>
-          <view v-if="post.tags" class="tags">
-            <template v-if="post.tags.length > 4">
-              <view class="tag">
-                {{ post.tags[0] }}
-              </view>
-              <view class="tag">
-                {{ post.tags[1] }}
-              </view>
-              <view class="tag">
-                {{ post.tags[2] }}
-              </view>
-            </template>
-            <template v-else>
-              <template v-for="tag in post.tags" :key="tag.id">
-                <view class="tag">
-                  {{ tag }}
-                </view>
-              </template>
-            </template>
-          </view>
-        </view>
-        <view
-          v-if="post.coverUrl"
-          :style="{ backgroundImage: 'url(' + post.coverUrl + ')' }"
-          class="image"
-        />
-      </view>
-      <view class="lower">
-        <view class="time font-sm">
-          {{ displayTime(post.createAt) }}
-        </view>
-        <view class="font-sm">{{ post.comments }}条回复</view>
-      </view>
-    </view>
-  </template>
+  <world-posts
+    v-if="isInitialized && !isRefreshing"
+    :search="{ type: 'default' }"
+  />
 
   <draft-button type="post" />
 </template>
 
 <script lang="ts" setup>
-import { reactive } from "vue";
-import { onPullDownRefresh, onReachBottom } from "@dcloudio/uni-app";
-import { onClickPost } from "./utils";
-import { getPostPreviews } from "@/apis/post/post";
+import { reactive, ref } from "vue";
+import { onPullDownRefresh } from "@dcloudio/uni-app";
 import DraftButton from "@/components/draft-button/draft-button.vue";
-import { displayTime } from "@/utils/time";
-import { Post } from "@/apis/schemas";
 
 import { init } from "@/utils/init";
+import WorldPosts from "@/pages/world/world-posts.vue";
 
-let postsData = reactive<Post[]>([]);
-let page = 0;
-const getPostPreviewsAsync = async () => {
-  const posts = (
-    await getPostPreviews({
-      page: page
-    })
-  ).posts;
-  if (posts.length === 0) {
-    uni.stopPullDownRefresh();
-  }
-  page++;
-  return posts;
-};
-
-async function createPostsDataBatch() {
-  const posts = await getPostPreviewsAsync();
-  postsData.push(...posts);
-}
-
-onReachBottom(() => {
-  createPostsDataBatch();
-});
+const isInitialized = ref(false);
 
 const types = reactive([
   {
@@ -163,19 +89,22 @@ const toggleSelf = (name: string) => {
   }
 };
 
+const isRefreshing = ref(false);
+
 function pageRefresh() {
-  postsData.splice(0);
-  page = 0;
-  createPostsDataBatch();
-  uni.stopPullDownRefresh();
+  isRefreshing.value = true;
+  setTimeout(() => {
+    isRefreshing.value = false;
+  }, 1);
 }
 
 onPullDownRefresh(() => {
   pageRefresh();
+  uni.stopPullDownRefresh();
 });
 
 init().then(() => {
-  createPostsDataBatch();
+  isInitialized.value = true;
 });
 </script>
 
@@ -232,100 +161,5 @@ body {
 
 .top-padding {
   height: calc(58 / 390 * 100vw);
-}
-
-.post {
-  background-color: #ffffff;
-  border-top: 1px #f5f5f5 solid;
-  border-bottom: 1px #f5f5f5 solid;
-  margin-bottom: calc(6 / 390 * 100vw);
-  padding: calc(16 / 390 * 100vw);
-}
-
-.upper {
-  display: flex;
-  align-items: center;
-}
-
-.main {
-  color: #353535;
-
-  &.hasImage {
-    width: calc(251 / 390 * 100vw);
-  }
-
-  .title {
-    font-size: calc(16 / 390 * 100vw);
-    font-weight: bold;
-    overflow: hidden;
-    -webkit-line-clamp: 1;
-    text-overflow: ellipsis;
-    display: -webkit-box;
-    -webkit-box-orient: vertical;
-  }
-
-  .description {
-    font-size: calc(12 / 390 * 100vw);
-    line-height: calc(17 / 390 * 100vw);
-    overflow: hidden;
-    -webkit-line-clamp: 2;
-    text-overflow: ellipsis;
-    display: -webkit-box;
-    -webkit-box-orient: vertical;
-  }
-
-  .tags {
-    display: flex;
-    flex-wrap: wrap;
-    color: #1fa1ff;
-    font-size: calc(10 / 390 * 100vw);
-    //height: calc(18 / 390 * 100vw);
-    //line-height: calc(18 / 390 * 100vw);
-    padding-top: 10rpx;
-
-    .tag {
-      margin-top: calc(4 / 390 * 100vw);
-      font-style: normal;
-      font-weight: bold;
-      font-size: calc(10 / 390 * 100vw);
-      line-height: calc(20 / 390 * 100vw);
-      /* or 170% */
-      text-align: center;
-      letter-spacing: calc(0.5 / 390 * 100vw);
-      /* blue02 */
-      color: #1fa1ff;
-
-      min-width: calc(28 / 390 * 100vw);
-      height: calc(20 / 390 * 100vw);
-      padding: calc(0 / 390 * 100vw) calc(8 / 390 * 100vw);
-      border: #1fa1ff calc(1 / 390 * 100vw) solid;
-      border-radius: calc(10 / 390 * 100vw);
-      margin-right: calc(8 / 390 * 100vw);
-    }
-  }
-}
-
-.image {
-  width: calc(142 / 390 * 100vw);
-  height: calc(93 / 390 * 100vw);
-  border-radius: calc(6 / 390 * 100vw);
-  margin-left: calc(16 / 390 * 100vw);
-  background-size: cover;
-  background-position: center;
-
-  //text-align: center;
-  //line-height: calc(93 / 390 * 100vw);
-}
-
-.lower {
-  margin-top: calc(12 / 390 * 100vw);
-  display: flex;
-  align-items: baseline;
-  color: #b8b8b8;
-  font-size: calc(10 / 390 * 100vw);
-
-  .time {
-    margin-right: calc(16 / 390 * 100vw);
-  }
 }
 </style>
