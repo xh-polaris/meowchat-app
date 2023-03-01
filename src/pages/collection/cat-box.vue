878a569f3c61ccd5fed252ead8648073533bf421
xx <78578210+chachachachachaxxxxx@users.noreply.github.com>
2023-03-01 22:18:07 +0800

    fix: 图鉴刷新bug修复 (#208)
    * fix: 修复图鉴页面的刷新bug
    
    * fix: 回退部分修改

--- src/pages/collection/cat-box.vue
+++ src/pages/collection/cat-box.vue
@@ -8,17 +8,17 @@
         <view class="name">
           {{ cat.name }}
         </view>
-        <view class="collect">
-          <image
-            :src="
-              cat.isCollected
-                ? '/static/images/collect.png'
-                : '/static/images/collect_HL.png'
-            "
-            mode="aspectFill"
-            @click.stop="onClickCollect"
-          />
-        </view>
+        <!--        <view class="collect">-->
+        <!--          <image-->
+        <!--            :src="-->
+        <!--              cat.isCollected-->
+        <!--                ? '/static/images/collect.png'-->
+        <!--                : '/static/images/collect_HL.png'-->
+        <!--            "-->
+        <!--            mode="aspectFill"-->
+        <!--            @click.stop="onClickCollect"-->
+        <!--          />-->
+        <!--        </view>-->
       </view>
       <view class="data">
         <text>花色：{{ cat.color }}</text>
@@ -33,7 +33,6 @@
 <script lang="ts" setup>
 import { reactive } from "vue";
 import { CatPreview } from "@/apis/schemas";
-import { onClickCollect } from "@/pages/collection/utils";
 
 const props = defineProps<{ cat: CatPreview }>();
 const cat = reactive(props.cat);
