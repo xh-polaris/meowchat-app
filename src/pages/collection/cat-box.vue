c738990ce940ce91a42bc593ab1702bf740f1327
oustr <111119927+oustr@users.noreply.github.com>
2023-03-12 05:38:11 +1100

    fix: 修复发动态选择猫咪样式崩溃问题 (#239)

--- src/pages/collection/cat-box.vue
+++ src/pages/collection/cat-box.vue
@@ -22,5 +22,54 @@ const cat = reactive(props.cat);
 </script>
 
 <style lang="scss" scoped>
-@import "@/common/cat-box.scss";
+.cats-box {
+  width: 700upx;
+  height: 200upx;
+  border-radius: 36upx;
+  display: flex;
+  align-items: center;
+  justify-content: left;
+  position: relative;
+  margin: 0 auto;
+  background-color: #ffffff;
+  box-shadow: 0 0 8upx rgba(0, 0, 0, 0.1);
+  margin-bottom: 12upx;
+  .avatar {
+    width: 174upx;
+    height: 174upx;
+    border-radius: 24upx;
+    margin-left: 24upx;
+    margin-right: 40upx;
+  }
+  .text {
+    flex: 1;
+    margin-right: 64upx;
+    height: 144upx;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    .title {
+      font-size: 36upx;
+      color: #1d1d1d;
+    }
+    .details {
+      font-size: 20upx;
+      color: #939393;
+      .info {
+      }
+    }
+  }
+  .star {
+    position: absolute;
+    right: 36upx;
+    top: 36upx;
+    width: 30upx;
+    height: 30upx;
+    background-size: 100% 100%;
+    background-image: url("/static/images/star-grey.png");
+    &.collected {
+      background-image: url("/static/images/star-yellow.png");
+    }
+  }
+}
 </style>
