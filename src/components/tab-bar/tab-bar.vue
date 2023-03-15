cf124a781d7d3afc827fedd5aa26846746fcecd6
zhy <2038290448@qq.com>
2023-03-15 23:18:45 +0800

    fix: 底部导航栏的高度提升1/10

--- src/components/tab-bar/tab-bar.vue
+++ src/components/tab-bar/tab-bar.vue
@@ -106,7 +106,7 @@ const tabSwitch = (path: string) => {
   z-index: 100;
   background: #fdfdfd;
   box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.2), 0 3px 10px 0 rgba(0, 0, 0, 0.19);
-  height: 120rpx;
+  height: 132rpx;
   .tab {
     display: grid;
   }
