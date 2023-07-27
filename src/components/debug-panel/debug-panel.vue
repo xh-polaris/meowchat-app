16b6a9a8b2f66bcc5100ec3fcfd710fd755ab733
oustr <biu_biu_@outlook.com>
2023-07-27 23:31:26 +0800

    feat: 初步实现"我的"页面调试按钮和面板切换

--- src/components/debug-panel/debug-panel.vue
+++ src/components/debug-panel/debug-panel.vue
@@ -1,18 +1,17 @@
 <template>
-  <view
-    v-if="!isDebugOn"
-    class="button"
-    @click="uni.setStorageSync('isDebugOn', true)"
-    >调试</view
-  >
-  <view v-else class="panel" @click="uni.setStorageSync('isDebugOn', false)"
-    >Panel</view
-  >
+  <view v-if="!isDebugOn" class="button" @click="setIsDebugOn(true)">调试</view>
+  <view v-else class="panel" @click="setIsDebugOn(false)">
+    <view>调试面板</view>
+  </view>
 </template>
 
 <script setup lang="ts">
 import { ref } from "vue";
 const isDebugOn = ref(uni.getStorageSync("isDebugOn"));
+const setIsDebugOn = ref((isTrue: boolean) => {
+  uni.setStorageSync("isDebugOn", isTrue);
+  isDebugOn.value = isTrue;
+});
 </script>
 
 <style scoped lang="scss">
@@ -30,5 +29,11 @@ const isDebugOn = ref(uni.getStorageSync("isDebugOn"));
   }
 }
 .panel {
+  padding: 2vw 4vw;
+  position: fixed;
+  left: 2vw;
+  bottom: 32vw;
+  background-color: #222222;
+  color: #cccccc;
 }
 </style>
