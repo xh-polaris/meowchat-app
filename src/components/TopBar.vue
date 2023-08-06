7007679c4e856320055e1b6d3619127bb2290205
oustr <biu_biu_@outlook.com>
2023-08-06 13:05:30 +0800

    feat: 实现TopBar的left和center双插槽

--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -2,7 +2,12 @@
   <view :style="{ height: navBarHeight + 'px' }" class="nav-bar">
     <view :style="{ height: topBarHeight + 'px' }" class="top-bar"></view>
     <view :style="{ height: capsuleBarHeight + 'px' }" class="capsule-bar">
-      <slot></slot>
+      <view class="center">
+        <slot name="center"></slot>
+      </view>
+      <view class="left">
+        <slot name="left"></slot>
+      </view>
     </view>
   </view>
   <view :style="{ height: navBarHeight + 'px' }"></view>
@@ -22,15 +27,19 @@ const navBarHeight = topBarHeight + capsuleBarHeight;
   background-color: #fafcff;
   width: 100vw;
   z-index: 100;
-}
+  .top-bar {
+    width: 100vw;
+  }
 
-.top-bar {
-  width: 100vw;
-}
-
-.capsule-bar {
-  width: 100vw;
-  display: flex;
-  align-items: center;
+  .capsule-bar {
+    width: 100vw;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    .left {
+      position: fixed;
+      left: 0;
+    }
+  }
 }
 </style>
