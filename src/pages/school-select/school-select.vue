7917de905881b3e2c6b6ec50e35ba43f7d758bfd
oustr <biu_biu_@outlook.com>
2023-08-07 14:40:36 +0800

    feat: 初步实现新的SearchBar组件

---
+++ src/pages/school-select/school-select.vue
@@ -0,0 +1,15 @@
+<template>
+  <TopBar :has-go-back="true">
+    <template #center>切换社区</template>
+  </TopBar>
+  <view style="height: 4vw"></view>
+  <SearchBar place-holder="输入学校名称"></SearchBar>
+  <view style="height: 4vw"></view>
+</template>
+
+<script setup lang="ts">
+import SearchBar from "@/components/SearchBar.vue";
+import TopBar from "@/components/TopBar.vue";
+</script>
+
+<style scoped lang="scss"></style>
