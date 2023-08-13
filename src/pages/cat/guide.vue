12305bf0423428d12a3463f773905c97a3c878c8
buluu97 <121414080+buluu97@users.noreply.github.com>
2023-08-13 23:09:18 +0800

    feat: 修改了guide页面 (#375)
    * feat: 修改了guide页面
    
    * chore: 修改了guide页面的格式
    
    * chore: 修改了guide页面的格式

--- src/pages/cat/guide.vue
+++ src/pages/cat/guide.vue
@@ -1,22 +1,88 @@
 <template>
-  <view class="main">
-    <text>guide</text>
-    <text>{{ props.cat.name }}</text>
+  <view class="guide-area">
+    <img :src="Icons.Guide" alt="撸猫指南" class="image-fill" />
+    <view class="text-view">
+      <text class="sub-heading">{{ subHeading }}</text>
+    </view>
+
+    <text>{{ props.cat.details }}</text>
   </view>
 </template>
-
-<script lang="ts" setup>
+<script setup lang="ts">
+import { ref } from "vue";
+import { Icons } from "@/utils/url";
+import { canvasToTempFilePath } from "@dcloudio/uni-h5"; // 引入图片链接
 import { Cat } from "@/apis/schemas";
-
+//const subHeading = "胆小怕生,不喜欢摸头";
 interface Props {
   cat: Cat;
 }
-
 const props = defineProps<Props>();
 </script>
 
-<style lang="scss" scoped>
-.main {
-  margin: 0 6vw;
+<style scoped lang="scss">
+.image-fill {
+  width: 100px;
+  height: 35px;
+  object-fit: cover;
+}
+
+.guide-area {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  background-color: rgb(250, 250, 250);
+
+  border-radius: 5px 5px;
+  border-radius: 20px;
+  width: 98%;
+
+  z-index: 0;
+}
+
+.title {
+  font-size: 20px;
+  font-weight: bold;
+  font-style: italic;
+  color: rgb(14, 171, 244);
+  font-family: "黑体", "微软雅黑", sans-serif;
+
+  margin-left: -15rpx;
+
+  z-index: 2;
+}
+
+.sub-heading {
+  font-weight: bold;
+  font-size: 15px;
+  font-family: "黑体", "微软雅黑", sans-serif;
+  margin-left: -5rpx;
+  position: relative;
+  z-index: 2;
+}
+
+.sub-heading::after {
+  content: "";
+  position: absolute;
+  bottom: 0rpx;
+
+  left: 10rpx;
+  width: 100%;
+  height: 10rpx;
+  background-color: #68bdfd;
+  transform: skewX(50deg);
+  z-index: -1;
+}
+
+.text {
+  font-size: 10px;
+  color: gray;
+
+  z-index: 2;
+}
+
+.text-view {
+  margin-top: 10rpx;
+  margin-left: 30rpx;
 }
 </style>
