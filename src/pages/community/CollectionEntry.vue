dde6bedcf5aa8f18e558217fe1284067d0a80a79
buluu97 <Cathy20040907@163.com>
2023-10-19 16:49:22 +0800

    feat: 修改了CollectionEntry.vue

--- src/pages/community/CollectionEntry.vue
+++ src/pages/community/CollectionEntry.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="image-container">
     <div class="cam">
-      <navigator open-type="navigate" url="/pages/collection/collection">
+      <navigator open-type="switchTab" url="/pages/collection/collection">
         <img :src="Icons.View" class="overlay-image view" />
       </navigator>
     </div>
 
     <div class="view">
-      <navigator open-type="navigate" url="/pages/collection/collection">
+      <navigator open-type="switchTab" url="/pages/collection/collection">
         <img :src="Icons.Cam" class="overlay-image cam" />
       </navigator>
     </div>
@@ -18,28 +18,31 @@
 import { Icons } from "@/utils/url";
 </script>
 
-<style>
+<style scoped lang="scss">
 .image-container {
   position: relative;
-  width: 90%;
-  height: 30%;
-}
-.overlay-image {
-  position: absolute;
   width: 90vw;
-  height: 18vw;
-}
-.cam {
-  top: 16vw;
-  left: 5vw;
-  width: 90vw;
-  z-index: 1;
-}
-.view {
-  top: 21vw;
-  left: 68vw;
-  width: 23vw;
-  z-index: 2;
-  height: 8.3vw;
+  height: 40vw;
+
+  .overlay-image {
+    position: absolute;
+    width: 90vw;
+    height: 18vw;
+
+    &.cam {
+      top: 16vw;
+      left: 5vw;
+      width: 90vw;
+      z-index: 1;
+    }
+
+    &.view {
+      top: 21vw;
+      left: 68vw;
+      width: 23vw;
+      z-index: 2;
+      height: 8.2vw;
+    }
+  }
 }
 </style>
