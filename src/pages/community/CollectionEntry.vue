237cf028cc92b7fd7763c23825bd9a5884902d19
buluu97 <121414080+buluu97@users.noreply.github.com>
2023-10-22 20:06:55 +0800

    chore: 修改了CollectionEntry.vue (#382)

--- src/pages/community/CollectionEntry.vue
+++ src/pages/community/CollectionEntry.vue
@@ -1,7 +1,48 @@
 <template>
-  <view></view>
+  <div class="image-container">
+    <div class="cam">
+      <navigator open-type="switchTab" url="/pages/collection/collection">
+        <img :src="Icons.View" class="overlay-image view" />
+      </navigator>
+    </div>
+
+    <div class="view">
+      <navigator open-type="switchTab" url="/pages/collection/collection">
+        <img :src="Icons.Cam" class="overlay-image cam" />
+      </navigator>
+    </div>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Icons } from "@/utils/url";
+</script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.image-container {
+  position: relative;
+  width: 90vw;
+  height: 40vw;
+
+  .overlay-image {
+    position: absolute;
+    width: 90vw;
+    height: 18vw;
+
+    &.cam {
+      top: 16vw;
+      left: 5vw;
+      width: 90vw;
+      z-index: 1;
+    }
+
+    &.view {
+      top: 21vw;
+      left: 68vw;
+      width: 23vw;
+      z-index: 2;
+      height: 8.2vw;
+    }
+  }
+}
+</style>
