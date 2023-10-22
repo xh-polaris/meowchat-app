7dd62ea12d42573948bf5c32ff413783a1406b58
python-No2 <103978188+python-No2@users.noreply.github.com>
2023-10-22 22:11:59 +0800

    feat: 轮播组件 (#384)

--- src/pages/community/CarouselTest.vue
+++ src/pages/community/CarouselTest.vue
@@ -1,7 +1,44 @@
 <template>
-  <view></view>
+  <view class="carouseltest">
+    <uni-swiper-dot
+      :info="info"
+      :current="current"
+      :field="content"
+      :mode="mode"
+      :dots-styles="dotsStyles"
+    >
+      <swiper class="swiper-box">
+        <swiper-item v-for="(item, index) in info()" :key="index">
+          <view class="swiper-item">
+            {{ item.content }}
+          </view>
+        </swiper-item>
+      </swiper>
+    </uni-swiper-dot>
+  </view>
 </template>
 
-<script setup lang="ts"></script>
+<script>
+//import uniSwiperDot from "@/components/uni-swiper-dot/uni-swiper-dot.vue";
+import { info } from "sass";
+//import Swiper from "swiper/bundle";
+//import "swiper/swiper-bundle.css";
+export default {
+  //components: { uniSwiperDot },
+  methods: {
+    info() {
+      return info;
+    },
+    change(e) {
+      this.current = e.detail.current;
+    }
+  }
+};
+</script>
 
-<style lang="scss" scoped></style>
+<style>
+.swiper-box {
+  border-radius: 0px;
+  padding: 0px;
+}
+</style>
