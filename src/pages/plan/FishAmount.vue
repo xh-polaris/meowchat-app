78702b7f5b1ad95eb1c8747c37659a14fdb8ee17
buluu97 <121414080+buluu97@users.noreply.github.com>
2023-11-04 20:55:48 +0800

    chore: 完善FishAmount.vue和GoToMyPlans.vue样式 (#6)

--- src/pages/plan/FishAmount.vue
+++ src/pages/plan/FishAmount.vue
@@ -1,5 +1,38 @@
-<template>test</template>
+<template>
+    <view class="goToMyPlans">
+        <img :src="Icons.LittleFish" class="small-icon" />
+        <text class="goTo_text">剩余<text style="color:dodgerblue">13</text>小鱼干</text>
+    </view>
+</template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Icons } from "@/utils/url";
+</script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.goToMyPlans {
+    border-radius: 2vw;
+    height: 12vw;
+    background-color: #feffff;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+
+    .small-icon {
+        height: 7vw;
+        width: 7vw;
+        margin-left: 3vw;
+        margin-right: 2vw;
+    }
+
+    .goTo_text {
+        color: #00365c;
+        font-size: 4vw;
+        margin-right: 2.5vw;
+        font-weight: bold;
+        letter-spacing: 0.3vw;
+    }
+
+
+}
+</style>
--- src/pages/plan/GoToMyPlans.vue
+++ src/pages/plan/GoToMyPlans.vue
@@ -1,7 +1,7 @@
 <template>
   <view class="goToMyPlans">
     <img :src="Icons.GoToPath_Tag" class="small-icon" />
-    <text class="goTo_text">我已助力了8个计划</text>
+      <text class="goTo_text">已助力了8个计划</text>
     <img :src="Icons.GoToArrow" class="arrow" />
   </view>
 </template>
@@ -18,23 +18,27 @@ import { Icons } from "@/utils/url";
   display: flex;
   flex-direction: row;
   align-items: center;
+  margin-right: 3.5vw;
 
   .small-icon {
-    height: 5vw;
-    width: 5vw;
-    margin-left: 3vw;
-    margin-right: 3vw;
+    height: 7vw;
+    width: 7vw;
+    margin-left: 2vw;
+    margin-right: 2vw;
   }
 
   .goTo_text {
     color: #00365c;
+    font-size: 4vw;
+    margin-right: 2vw;
+    font-weight: bold;
+    letter-spacing: 0.3vw;
   }
 
   .arrow {
-    height: 5vw;
-    width: 3vw;
-    position: absolute;
-    right: 10vw;
+    height: 3vw;
+    width: 1.8vw;
+    margin-right: 2.5vw;
   }
 }
 </style>
