76be3697f11b8f20cd867a4d58e7632844c62a82
python-No2 <103978188+python-No2@users.noreply.github.com>
2023-11-06 22:30:27 +0800

    chore: 实现了小鱼干计划详情的助力按钮 (#9)

--- src/pages/plan/plan-details/BottomBar.vue
+++ src/pages/plan/plan-details/BottomBar.vue
@@ -1,5 +1,40 @@
-<template>底部 需要position: fixed对齐页面底部</template>
-
-<script setup lang="ts"></script>
-
-<style scoped lang="scss"></style>
+<template>
+  <view class="bottom-bar">
+    <view class="help-button" @click="onHelpClick">
+      <view class="help-text">马上助力</view>
+    </view>
+  </view>
+</template>
+<script setup lang="ts">
+function onHelpClick() {
+  console.log("助力按钮被点击");
+}
+</script>
+<style scoped lang="scss">
+.bottom-bar {
+  position: fixed;
+  bottom: 0;
+  background-color: #ffffff;
+  width: 100vw;
+  height: 20vw;
+  .help-button {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    bottom: 0;
+    background-color: #1e97ff;
+    width: 50vw;
+    height: 10vw;
+    border-radius: 10vw;
+    margin-left: 25vw;
+    margin-top: 2vw;
+    .help-text {
+      text-align: center;
+      font-weight: bold;
+      font-size: 4.5vw;
+      letter-spacing: 0.5vw;
+      color: #ffffff;
+    }
+  }
+}
+</style>
