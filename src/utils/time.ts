bcd137377d8bc985bf09b5568bad90009f544356
oustr <111119927+oustr@users.noreply.github.com>
2023-12-03 01:34:50 +0800

    perf: 点击id复制到剪贴板 (#385)
    * chore: 点击id复制到剪贴板
    
    * chore: 时间戳自适应 数据对接换成instruction

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -1,13 +1,16 @@
 import moment from "moment";
 import "moment/dist/locale/zh-cn";
 
-export function displayTime(secTime: number) {
-  secTime *= 1000; // 后端传回的时间戳是秒级的，需要乘以1000变为微秒级，因为JS的时间戳是微秒级的
-  const now = new Date().getTime();
-  if (now < secTime) {
-    secTime = now;
+export function displayTime(timeStamp: number) {
+  if (timeStamp < 1e11) {
+    timeStamp *= 1000; // 后端传回的时间戳是秒级的，需要乘以1000变为微秒级，因为JS的时间戳是微秒级的
   }
-  const time = moment(secTime);
+
+  const now = new Date().getTime();
+  if (now < timeStamp) {
+    timeStamp = now;
+  }
+  const time = moment(timeStamp);
   if (time.year() != moment(now).year()) {
     return time.format("YYYY-MM-DD");
   } else {
